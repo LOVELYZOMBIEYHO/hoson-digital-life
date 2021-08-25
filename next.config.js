92d1f7f25fe031848a0fce08686c098394b6a618
LOVELYZOMBIEYHO <53188095+LOVELYZOMBIEYHO@users.noreply.github.com>
2021-08-25 19:33:54 +0800

    eslint debug close

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
     domains: ["res.cloudinary.com"],
     // domains: ["strapi-it-blog.herokuapp.com"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
